42854ac653c6992c7cea6549d2b3db0bb1d36945
denyrachmat22 <denyrachmat22@gmail.com>
2024-01-28 01:03:12 +0700

    Fix: Change line

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -473,11 +473,11 @@ class DeliveryController extends Controller
             $this->fpdf->SetFont('Arial', '', 10);
             $this->fpdf->SetXY(150, 10);
             $this->fpdf->MultiCell(55, 4, 'Kepada ' . $RSHeader->MCUS_CUSNM, 0, 'L');
-            $this->fpdf->SetXY(150, 15);
-            $this->fpdf->MultiCell(55, 4, $RSHeader->MCUS_ADDR1, 0, 'L');
-            $this->fpdf->SetXY(150, 20);
-            $this->fpdf->MultiCell(55, 4, $RSHeader->MCUS_REFF_MKT, 0, 'L');
             $this->fpdf->SetXY(150, 25);
+            $this->fpdf->MultiCell(55, 4, $RSHeader->MCUS_ADDR1, 0, 'L');
+            $this->fpdf->SetXY(150, 30);
+            $this->fpdf->MultiCell(55, 4, $RSHeader->MCUS_REFF_MKT, 0, 'L');
+            $this->fpdf->SetXY(150, 45);
             $this->fpdf->MultiCell(55, 4, $RSHeader->MCUS_TELNO, 0, 'L');
 
             $this->fpdf->SetFont('Arial', 'U', 10);

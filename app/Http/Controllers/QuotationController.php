c654011f1ccc3c4b28da8a1e46b1b32877ce46f5
denyrachmat22 <denyrachmat22@gmail.com>
2024-01-27 22:16:10 +0700

    Fix: Delete footer

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -1242,28 +1242,6 @@ class QuotationController extends Controller
             }
         }
 
-        $branchPaymentAccount = BranchPaymentAccount::on($this->dedicatedConnection)
-            ->where('BRANCH', Auth::user()->branch)
-            ->whereNull('deleted_at')
-            ->get();
-
-        $y += 15;
-        $this->fpdf->SetFont('Arial', 'B', 10);
-        $this->fpdf->SetXY(6, $y);
-        $this->fpdf->Cell(80, 5, 'BANK', 1, 0, 'C');
-        $this->fpdf->Cell(70, 5, 'Atas Nama', 1, 0, 'C');
-        $this->fpdf->Cell(50, 5, 'Nomor Rekening', 1, 0, 'C');
-
-        $y += 5;
-        $this->fpdf->SetFont('Arial', '', 10);
-        foreach ($branchPaymentAccount as $r) {
-            $this->fpdf->SetXY(6, $y);
-            $this->fpdf->Cell(80, 5, $r->bank_name, 1, 0, 'C');
-            $this->fpdf->Cell(70, 5, $r->bank_account_name, 1, 0, 'C');
-            $this->fpdf->Cell(50, 5, $r->bank_account_number, 1, 0, 'C');
-            $y += 5;
-        }
-
         $y += 10;
         $this->fpdf->SetXY(7, $y);
         $this->fpdf->Cell(20, 5, 'Hormat kami,', 0, 0, 'L');

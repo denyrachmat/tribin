12d560c3f7502202197f2d0e1c7f62e618760137
denyrachmat22 <denyrachmat22@gmail.com>
2024-02-27 10:41:07 +0700

    Fix: Change to view

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -327,7 +327,7 @@ class QuotationController extends Controller
         $documentNumber = base64_decode($id);
 
         $RS = T_QUODETA::on($this->dedicatedConnection)->select(["id", "TQUODETA_ITMCD", "MITM_ITMNM", "TQUODETA_USAGE_DESCRIPTION", "TQUODETA_PRC", "TQUODETA_OPRPRC", "TQUODETA_MOBDEMOB", 'TQUODETA_ITMQT', 'TQUODETA_ELECTRICITY'])
-            ->leftJoin("M_ITM", function ($join) {
+            ->leftJoin("M_ITM_GRP", function ($join) {
                 $join->on("TQUODETA_ITMCD", "=", "MITM_ITMCD")
                     ->on('TQUODETA_BRANCH', '=', 'MITM_BRANCH');
             })

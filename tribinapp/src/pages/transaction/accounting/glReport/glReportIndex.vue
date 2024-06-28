c84a86e1c4bced4a1d10d3176bbb752c76e5d477
denyrachmat22 <denyrachmat22@gmail.com>
2024-06-28 18:12:02 +0700

    Fix: Add GL Report

---
+++ tribinapp/src/pages/transaction/accounting/glReport/glReportIndex.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="q-pa-md">
+    <div class="row q-pb-md">
+      <div class="col">
+        <span class="text-h4">GL Report</span>
+      </div>
+      <div class="col text-right">
+      </div>
+    </div>
+
+    <q-separator />
+
+    <div class="row q-pt-md">
+      <div class="col"></div>
+    </div>
+  </div>
+</template>
+<script setup></script>

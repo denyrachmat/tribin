a31aa90013faa6eabe85d01e7bb8f78b6247ba72
denyrachmat22 <denyrachmat22@gmail.com>
2024-07-20 01:15:46 +0700

    Fix: Update to base64

---
+++ tribinapp/src/pages/transaction/pos/sales/posSalesIndex.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="q-pa-md bg-grey">
+    <div class="row q-pb-md">
+      <div class="col">
+        <span class="text-h4">POS</span>
+      </div>
+      <div class="col text-right"></div>
+    </div>
+
+    <q-separator />
+
+    <div class="row q-col-gutter-md q-pt-md">
+      <div class="col-12 col-sm-4">
+        <div class="row">
+          <div class="col">
+            <q-input filled v-model="date">
+              <template v-slot:prepend>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </div>
+        <div class="row q-pt-md">
+          <div class="col">
+            <q-input filled v-model="date">
+              <template v-slot:prepend>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>

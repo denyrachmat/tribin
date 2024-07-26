25218cdf03ea09380d816c8c2aef85c1a6e07ddd
denyrachmat22 <denyrachmat22@gmail.com>
2024-07-26 07:01:10 +0700

    Fix: Update POS Lagi

---
+++ public/assets/use-quasar.13b18fbe.js
@@ -0,0 +1,1 @@
+import{i as a,ai as r}from"./index.12d4ec66.js";function u(){return a(r)}export{u};
---
+++ public/assets/use-quasar.dbb032b0.js
@@ -0,0 +1,1 @@
+import{i as a,ai as r}from"./index.2154091f.js";function u(){return a(r)}export{u};

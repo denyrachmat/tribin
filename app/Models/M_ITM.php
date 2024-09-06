5b9ea8566bcbe86223eeb5dc7d22f3f17d92a929
denyrachmat22 <denyrachmat22@gmail.com>
2024-09-06 10:43:45 +0700

    Fix: Update model MITM

--- app/Models/M_ITM.php
+++ app/Models/M_ITM.php
@@ -9,5 +9,7 @@ class M_ITM extends Model
 {
     use HasFactory;
     protected $table = 'M_ITM';
+    protected $primaryKey = "MITM_ITMCD";
+    public $incrementing = false;
     protected $fillable = ['MITM_ITMCD', 'MITM_ITMNM', 'MITM_ITMTYPE', 'MITM_STKUOM', 'MITM_BRAND', 'MITM_MODEL', 'MITM_SPEC', 'MITM_ITMCAT', 'MITM_COACD', 'MITM_BRANCH'];
 }

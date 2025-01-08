266e0d2d2719219e4cf6f937bff13cb3a698a43c
denyrachmat22 <denyrachmat22@gmail.com>
2025-01-08 15:58:59 +0700

    Fix; Update

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -140,8 +140,8 @@ class ItemController extends Controller
     function searchAPI(Request $request)
     {
         $columnMap = [
-            DB::raw('M_ITM_GRP.MITM_ITMNM'),
-            DB::raw("M_ITM_GRP.MITM_ITMNMREAL"),
+            DB::raw('M_ITM_GRP.MITM_ITMNM as MITM_ITMNM'),
+            DB::raw("M_ITM_GRP.MITM_ITMNMREAL as MITM_ITMNMREAL"),
             'LATEST_PRC',
             'STOCK'
         ];

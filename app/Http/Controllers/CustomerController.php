a7ae0bb5e2cf2d3eca4f20c45edb7a8587033f06
denyrachmat22 <denyrachmat22@gmail.com>
2025-01-21 10:02:56 +0700

    Fix: Fixing collate again

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -35,11 +35,11 @@ class CustomerController extends Controller
         $RS = DB::connection($request->fromConnection)->table('M_CUS AS A')
             ->select('A.*')
             // ->leftJoin(DB::raw($currentDBName . '.M_CUS AS B COLLATE utf8mb4_unicode_ci'), 'A.MCUS_CUSCD', '=', DB::raw('B.MCUS_CUSCD COLLATE utf8mb4_unicode_ci'))
-            ->leftJoin(DB::raw($currentDBName . '.M_CUS AS B'), function ($join) {
-                $join->on('A.MCUS_CUSCD', '=', DB::raw('B.MCUS_CUSCD COLLATE utf8mb4_unicode_ci'))
-                     ->whereColumn('A.MCUS_CUSCD', 'COLLATE', 'utf8mb4_unicode_ci');
+            ->leftJoin($currentDBName . '.M_CUS as B', function ($join) {
+                $join->on('A.MCUS_CUSCD', '=', DB::raw('B.MCUS_CUSCD COLLATE utf8mb4_unicode_ci'));
             })
             ->where('A.MCUS_BRANCH',  Auth::user()->branch)
+            ->where('A.MCUS_CUSCD', 'COLLATE', 'utf8mb4_unicode_ci') // Apply COLLATE to A.MCUS_CUSCD in the WHERE clause
             ->whereNull('B.MCUS_CUSCD');
         $RSTosave = json_decode(json_encode($RS->get()), true);
         if (!empty($RSTosave)) {

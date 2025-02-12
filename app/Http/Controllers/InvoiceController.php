4f083ac3424a243a6dc29db5f1aba6ced3c10d95
denyrachmat22 <denyrachmat22@gmail.com>
2025-02-12 17:01:40 +0700

    Fix: Update printKwitansi method to enhance item code filtering and improve layout formatting

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -461,7 +461,7 @@ class InvoiceController extends Controller
             )
                 ->leftjoin('M_ITM_GRP', 'TSLODETA_ITMCD', 'MITM_ITMNM')
                 ->where('TSLODETA_SLOCD', $r->TDLVORDDETA_SLOCD)
-                ->where('TSLODETA_ITMCD', $r->TDLVORDDETA_ITMCD)
+                ->where('TSLODETA_ITMCD','like', '%'.$r->TDLVORDDETA_ITMCD.'%')
                 ->where('TSLODETA_BRANCH', Auth::user()->branch)
                 // ->where('MITM_ITMTYPE', 1)
                 ->first();
@@ -562,20 +562,22 @@ class InvoiceController extends Controller
         $Yfocus += 6;
         $this->fpdf->SetXY(10, $Yfocus);
         $this->fpdf->Cell(50, 5, 'Lokasi', 0, 0, 'L');
-        $this->fpdf->Cell(50, 5, ': ' . (!empty($Subject) ? $Subject->TQUO_PROJECT_LOCATION : ''), 0, 0, 'L');
-        $this->fpdf->Line(63, $Yfocus + 5, 150, $Yfocus + 5);
-        $Yfocus += 5;
+        $this->fpdf->Cell(2, 5, ':');
+        $this->fpdf->MultiCell(138, 5, (!empty($Subject) ? $Subject->TQUO_PROJECT_LOCATION : ''));
+        $this->fpdf->Line(63, $this->fpdf->GetY() + 2, 180, $this->fpdf->GetY() + 2);
+        $Yfocus = $this->fpdf->GetY() + 5;
+        // $Yfocus += 5;
         $this->fpdf->SetXY(110, $Yfocus);
         $this->fpdf->Cell(50, 5, $Branch->MBRANCH_NM . ', ' . $DOIssuDate, 0, 0, 'L');
-        $Yfocus += 10;
+        $Yfocus = $this->fpdf->GetY() + 10;
         $this->fpdf->SetXY(10, $Yfocus);
         $this->fpdf->Cell(50, 5, 'Jumlah', 0, 0, 'L');
         $this->fpdf->Cell(50, 5, ': Rp. ' . number_format($PPNAmount + $totalHargaSewa), 0, 0, 'L');
-        $Yfocus += 25;
+        $Yfocus = $this->fpdf->GetY() + 10;
         $this->fpdf->SetXY(120, $Yfocus);
         $this->fpdf->Cell(50, 5, 'Syapril, S.T', 0, 0, 'L');
-        $Yfocus += 9;
-        $this->fpdf->SetXY(6, $Yfocus);
+        // $Yfocus += 9;
+        $this->fpdf->SetXY(6, 145);
         $this->fpdf->SetFont('Arial', '', 8);
         $this->fpdf->Cell(50, 5, 'Note: Pembayaran dengan Giro/Cheque/Transfer dianggap sah apabila dan sudah masuk ke rekening kami', 0, 0, 'L');
 

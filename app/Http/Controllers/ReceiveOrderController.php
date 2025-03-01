25ddb950388cb1504076b68f486a0fac3fb511ef
denyrachmat22 <denyrachmat22@gmail.com>
2025-03-01 12:13:11 +0700

    Fix: Store last line number when recreating document code

--- app/Http/Controllers/ReceiveOrderController.php
+++ app/Http/Controllers/ReceiveOrderController.php
@@ -287,6 +287,7 @@ class ReceiveOrderController extends Controller
             if ($request->isRecreate) {
                 $cekSLOData = T_SLOHEAD::on($this->dedicatedConnection)->where('TSLO_SLOCD', 'like', $request->TSLO_SLOCD . '%')->orderBy('created_at', 'desc')->first();
                 $newDocumentCode =  $request->TSLO_SLOCD . (str_contains($cekSLOData->TSLO_SLOCD, '-') ? '-' . sprintf('%03d', (int) substr($cekSLOData->TSLO_SLOCD, -3) + 1) :  '-001');
+                $LastLine = $cekSLOData->TSLO_LINE;
             } else {
                 $newDocumentCode =  $request->TSLO_SLOCD;
             }

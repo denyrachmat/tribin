3f4254092b40e460ef8e02c1e18f56a3f556ebac
denyrachmat22 <denyrachmat22@gmail.com>
2025-06-09 18:32:46 +0700

    Fix: Update button labels and add new navigation functions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.111.3">
     <title>Welcome · JOS</title>
-    <link href="{{ url('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">    
+    <link href="{{ url('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <meta name="theme-color" content="#712cf9">
 
 
@@ -126,7 +126,9 @@
                         <p class="text-body-secondary">JOS stands for JAT Integrated Online System</p>
                         <p>
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary" onclick="enter_jat_power(this)">Enter</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" onclick="enter_jat_power(this)">Bussiness Application</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" onclick="enter_jat_acc(this)">Accounting</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" onclick="enter_jat_payroll(this)">Payroll</button>
                         </div>
                         </p>
                     </div>
@@ -182,7 +184,7 @@
                             </svg>
                             <div class="card-body">
                                 <p class="card-text">
-                                <div class="d-flex justify-content-between align-items-center">                                    
+                                <div class="d-flex justify-content-between align-items-center">
                                     <small class="text-body-secondary"></small>
                                 </div>
                             </div>
@@ -196,7 +198,7 @@
                             </svg>
                             <div class="card-body">
                                 <p class="card-text"></p>
-                                <div class="d-flex justify-content-between align-items-center">                                   
+                                <div class="d-flex justify-content-between align-items-center">
                                     <small class="text-body-secondary"></small>
                                 </div>
                             </div>
@@ -210,7 +212,7 @@
                             </svg>
                             <div class="card-body">
                                 <p class="card-text"></p>
-                                <div class="d-flex justify-content-between align-items-center">                                    
+                                <div class="d-flex justify-content-between align-items-center">
                                     <small class="text-body-secondary"></small>
                                 </div>
                             </div>
@@ -224,7 +226,7 @@
                             </svg>
                             <div class="card-body">
                                 <p class="card-text"></p>
-                                <div class="d-flex justify-content-between align-items-center">                                    
+                                <div class="d-flex justify-content-between align-items-center">
                                     <small class="text-body-secondary"></small>
                                 </div>
                             </div>
@@ -247,6 +249,14 @@
     <script>
         function enter_jat_power(pthis) {
             window.location.href = '/welcome'
+        }
+
+        function enter_jat_acc(pthis) {
+            window.open('https://acc.jatpowerindo.co.id', '_blank');
+        }
+
+        function enter_jat_payroll(pthis) {
+            window.open('https://payroll.jatpowerindo.co.id', '_blank');
         }
     </script>
 

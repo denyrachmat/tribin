d05e9c07689a096fba42829b278bf4b11b7d6bf9
denyrachmat22 <denyrachmat22@gmail.com>
2026-02-02 15:23:16 +0700

    feat: enhance searchAPI to include formatted customer name and address

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -181,7 +181,7 @@ class CustomerController extends Controller
 
     function searchAPI(Request $request)
     {
-        $RSTemp = M_CUS::on($this->dedicatedConnection)->select('*')
+        $RSTemp = M_CUS::on($this->dedicatedConnection)->select('*', DB::raw('CONCAT(MCUS_CUSNM, " - ", RTRIM(MCUS_ADDR1, 15)) AS MCUS_CUSNM'))
             ->where('MCUS_BRANCH', Auth::user()->branch);
 
         if ($request->has('type')) {

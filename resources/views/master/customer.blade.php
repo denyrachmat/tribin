7ca8998d62295f05d4ccd34b314df27f5a2b3ebb
denyrachmat22 <denyrachmat22@gmail.com>
2026-05-20 14:49:45 +0700

    Fix: Add 'Internal' customer type option in customer form for better categorization

--- resources/views/master/customer.blade.php
+++ resources/views/master/customer.blade.php
@@ -26,6 +26,10 @@
                 <input class="form-check-input" type="radio" name="customerType" id="radio2" value="2">
                 <label class="form-check-label" for="radio2">Corporate</label>
             </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="customerType" id="radio3" value="3">
+                <label class="form-check-label" for="radio3">Internal</label>
+            </div>
         </div>
         <div class="col-md-6 mb-1">
             <div class="input-group input-group-sm mb-1">

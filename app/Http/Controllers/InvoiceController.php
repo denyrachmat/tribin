46dd70b8595b22d1ff9f4ef7fefebe563bba1341
denyrachmat22 <denyrachmat22@gmail.com>
2026-08-13 18:22:14 +0700

    Fix: Improve driver profile retrieval in printSJ method to handle empty driver names

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -1863,7 +1863,7 @@ class InvoiceController extends Controller
             $this->fpdf->SetXY(15, $startCond + 3);
 
             $getDriverSPK = $RSHeader->spk->where('CSPK_PIC_AS', 'DRIVER')->first();
-            $getDriverProfile = HRMEmployee::where('employee_id', $getDriverSPK->CSPK_PIC_NAME)->first();
+            $getDriverProfile = !empty($getDriverSPK->CSPK_PIC_NAME) ? HRMEmployee::where('employee_id', $getDriverSPK->CSPK_PIC_NAME)->first() : '';
             $getDriverProfile = !empty($getDriverProfile) ? $getDriverProfile->toArray() : ['full_name' => ''];
 
             if (str_contains($RSHeader->TDLVSJDETA_TYPE, 'forklift')) {

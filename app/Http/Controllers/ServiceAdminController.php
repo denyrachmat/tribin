7dc44f73ee9e65f7d0bd7bd02094a1b70e9fef82
denyrachmat22 <denyrachmat22@gmail.com>
2026-08-29 09:26:33 +0700

    Feat: Update confirmDoneItem to join T_LOC_REQ and use approved date for TDLVORD_ISSUDT

--- app/Http/Controllers/ServiceAdminController.php
+++ app/Http/Controllers/ServiceAdminController.php
@@ -222,6 +222,7 @@ class ServiceAdminController extends Controller
             } else {
                 $cekDataAll = (clone $hasil)->get()->toArray();
                 $doc = T_SRV_HEAD::on($this->dedicatedConnection)->join('T_SRV_DET', 'T_SRV_HEAD.id', 'TSRVH_ID')
+                    ->join('T_LOC_REQ', 'TLOCREQ_DOCNO', DB::raw("CONCAT(T_SRV_HEAD.SRVH_DOCNO, '-', T_SRV_DET.TSRVD_LINE)"))
                     ->where('T_SRV_DET.id', base64_decode($id))
                     ->first();
 
@@ -252,7 +253,7 @@ class ServiceAdminController extends Controller
                 $createReq = new Request([
                     'TDLVORD_DLVCD' => $doc->SRVH_DOCNO . '-' . $doc->TSRVD_LINE,
                     'TDLVORD_CUSCD' => $doc->SRVH_CUSCD,
-                    'TDLVORD_ISSUDT' => $doc->SRVH_ISSDT,
+                    'TDLVORD_ISSUDT' => $doc->TLOCREQ_APPRVDT,
                     'TDLVORD_REMARK' => 'SERVICE-INTERNAL',
                     'typeOutgoing' => 4,
                     'SO_DET' => $listForDODet,

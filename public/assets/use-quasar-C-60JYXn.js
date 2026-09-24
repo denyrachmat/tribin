0dc707792bb26823c578063b7a55bb7a323e8fd2
denyrachmat22 <denyrachmat22@gmail.com>
2026-09-24 14:51:58 +0700

    chore: stop tracking compiled public/assets bundles

--- public/assets/use-quasar-C-60JYXn.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aY as r}from"./index-DXJZaBe0.js";function u(){return a(r)}export{u};
--- public/assets/use-quasar-CL7NDYr6.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aY as r}from"./index-DdV68LkF.js";function u(){return a(r)}export{u};
